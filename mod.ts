3bd7a0b3f6b164bb5abcc252b6b1136cb07f0df4
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-04-24 23:04:47 +0200

    initial commit

---
+++ mod.ts
@@ -0,0 +1,43 @@
+const describeTag = await Deno.spawn("git", {
+  args: [
+    "describe",
+    "--tags",
+    "--abbrev=0",
+    "HEAD^",
+  ],
+});
+
+const tag = new TextDecoder().decode(describeTag.stdout);
+
+const { status, stdout, stderr } = await Deno.spawn("git", {
+  args: [
+    "log",
+    '--pretty=format:"%s"', // print commit message only
+    `${tag.replace(/\s/g, "")}..HEAD`,
+  ],
+});
+
+if (!status.success) {
+  const err = new TextDecoder().decode(stderr);
+  throw new Error(`subprocess failed: ${err}`);
+}
+
+// <type>[optional scope]: <description>
+
+// [optional body]
+
+// [optional footer(s)]
+
+const commits = new TextDecoder().decode(stdout).split("\n");
+
+// split commit by newline, get first and last element?
+
+const conventialCommitRegex =
+  /^"(?<type>\w+?)(?<scope>\(\w*?\))?(?<breaking>!)?:(?<desc>.*?)$(?:\n\n(?<body>.*))?(?:$\n\n(?<footer>.*)$)?/ms;
+
+for (const commit of commits) {
+  console.log(commit.split("\n"));
+  const patchCommit = commit.match(conventialCommitRegex);
+
+  //console.log(patchCommit);
+}
