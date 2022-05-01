adfbf28af41cfbc00a6b50a4c8261e5f549eaf32
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-05-01 11:08:04 +0200

    feat(experimental): add prerelease option (#23)

---
+++ cli.ts
@@ -0,0 +1,23 @@
+import { Command } from "./deps.ts";
+import { main } from "./mod.ts";
+
+export interface Options {
+  prerelease?: true;
+}
+
+await new Command()
+  .name("denotation")
+  .version("0.2.2")
+  .description("Create semantic releases based on conventional commit history.")
+  .option(
+    "--prerelease",
+    "Sets whether or not the next version should be a pre-release",
+  )
+  .action(async (options) => {
+    if (options.prerelease) {
+      console.warn("Warning: '--prerelease' is an experimental option.");
+    }
+
+    await main(options);
+  })
+  .parse(Deno.args);
--- deps.ts
+++ deps.ts
@@ -1,1 +1,2 @@
+export { Command } from "https://deno.land/x/cliffy@v0.23.2/command/mod.ts";
 export { writeAll } from "https://deno.land/std@0.136.0/streams/mod.ts";
