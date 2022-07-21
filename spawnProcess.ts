373bcbe96b89fcc6ef797fdfa37edd5f56273e5b
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-07-21 19:55:18 +0200

    fix: update spawn api for deno v1.24

--- spawnProcess.ts
+++ spawnProcess.ts
@@ -3,11 +3,11 @@ export async function spawnProcess(
   command: string,
   args: string[],
 ): Promise<Uint8Array> {
-  const { status, stdout, stderr } = await Deno.spawn(command, {
+  const { success, stdout, stderr } = await Deno.spawn(command, {
     args: args,
   });
 
-  if (!status.success) {
+  if (!success) {
     const err = new TextDecoder().decode(stderr);
     throw new Error(`${command} failed: ${err}`);
   }
